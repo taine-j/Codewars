bd7ecca1d68d407c1f8455ccfa6a7d8579131873
Taine Jarvis <tainejarvis.dev@gmail.com>
2023-04-08 11:15:57 +0100

    Code Wars for 8-4-2023

---
+++ twoToOne.js
@@ -0,0 +1,29 @@
+/* DESCRIPTION:
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" */
+
+
+
+
+
+
+
+function longest(s1, s2) {
+    let output = [];
+    let combi = s1.concat(s2);
+    let array = combi.split('').sort();
+ 
+    for (let i = 0; i <= array.length; i++) {
+        console.log(array[i]);//test
+        if (!output.includes(array[i]) ) {
+              output.push(array[i]);
+        }
+    }
+    return output.join('');}
