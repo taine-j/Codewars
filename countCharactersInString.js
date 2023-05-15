2569d3bd15c3f523c1d2096da1c25694f704443a
Taine Jarvis <tainejarvis@Taines-MacBook-Air.local>
2023-05-15 17:40:36 +0100

    codewars for 15-5-2023

---
+++ countCharactersInString.js
@@ -0,0 +1,17 @@
+/* The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+What if the string is empty? Then the result should be empty object literal, {}.
+ */
+
+function count (string) {
+    var result = {};
+    
+    for(let i = 0; i < string.length; i++) {
+      if(result.hasOwnProperty(string[i])){
+        result[string[i]] += 1;
+      } else {
+        result[string[i]] = 1;
+      }
+    }
+    
+    return result;
+  }
