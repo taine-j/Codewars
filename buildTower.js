27177b5f324ec0ebd91af62d80cbd0bfd4c25016
Taine Jarvis <tainejarvis.dev@gmail.com>
2023-06-16 11:32:52 +0100

    codewars for 16-6-2023

---
+++ buildTower.js
@@ -0,0 +1,33 @@
+/* DESCRIPTION:
+Build Tower
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+]
+ */
+
+
+function towerBuilder(nFloors) {
+    var tower = [];
+    for (var i = 0; i < nFloors; i++) {
+      tower.push(" ".repeat(nFloors - i - 1)
+               + "*".repeat((i * 2)+ 1)
+               + " ".repeat(nFloors - i - 1));
+    }
+    return tower;
+  }
