04db1657c484ad9b1856d8a807958d8e8566b44b
Taine Jarvis <tainejarvis.dev@gmail.com>
2023-06-23 14:07:38 +0100

    codewars for 23-6-23

--- buildTower.js
+++ buildTower.js
@@ -24,7 +24,7 @@ And a tower with 6 floors looks like this:
 
 function towerBuilder(nFloors) {
     var tower = [];
-    for (var i = 0; i < nFloors; i++) {
+    for (let i = 0; i < nFloors; i++) {
       tower.push(" ".repeat(nFloors - i - 1)
                + "*".repeat((i * 2)+ 1)
                + " ".repeat(nFloors - i - 1));
