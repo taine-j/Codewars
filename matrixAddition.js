06b189da8ff6b39a57752904d4e350c0c0815200
Taine Jarvis <tainejarvis.dev@gmail.com>
2023-09-05 09:58:59 +0100

    codewars for 5-9-2023

---
+++ matrixAddition.js
@@ -0,0 +1,22 @@
+/* DESCRIPTION:
+Write a function that accepts two square matrices (N x N two dimensional arrays), and return the sum of the two. Both matrices being passed into the function will be of size N x N (square), containing only integers.
+
+How to sum two matrices:
+
+Take each cell [n][m] from the first matrix, and add it with the same [n][m] cell from the second matrix. This will be cell [n][m] of the solution matrix. (Except for C where solution matrix will be a 1d pseudo-multidimensional array).
+
+Visualization:
+
+|1 2 3|     |2 2 1|     |1+2 2+2 3+1|     |3 4 4|
+|3 2 1|  +  |3 2 3|  =  |3+3 2+2 1+3|  =  |6 4 4|
+|1 1 1|     |1 1 3|     |1+1 1+1 1+3|     |2 2 4| */
+
+const matrixAddition = (a, b) => {
+    const n = a.length;
+    const m = a[0].length;
+    for (let i = 0; i < n; i++)
+      for (let j = 0; j < m; j++)
+        a[i][j] += b[i][j];
+    return a;    
+  }
+  
