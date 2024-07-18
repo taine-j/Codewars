1087b73d6df1272a3121af21073dcc04d0db1cf4
Taine Jarvis <tainejarvis.dev@gmail.com>
2024-07-18 09:22:26 +0100

    18/07/2024

---
+++ multiples3or5.ts
@@ -0,0 +1,29 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+// Additionally, if the number is negative, return 0.
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+export class Challenge {
+    static solution(number: number) {
+      
+      let multiples:number[] = []; 
+      
+      for (let i=0; i<number; i++){
+        
+        if (i < 0){return 0}
+        
+        else if (i % 3 === 0 || i % 5 === 0){
+          multiples.push(i)
+        }
+      }
+      
+      console.log(multiples)
+      
+      return multiples.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+  },0);
+      
+    }}
